f3a205c3e05908cbccddc02a052f35f9284c3bcf
M1xon0 <cmi1.mikolaj@gmail.com>
2026-03-30 00:11:56 +0200

    Create App.vue file with ClipSMP branding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="branding">
+    <h1 class="title">ClipSMP</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+.branding {
+  text-align: center;
+  color: #32ff96;
+  background: rgba(255, 50, 200, 0.3);
+  font-family: 'Press Start 2P', cursive;
+}
+</style>
